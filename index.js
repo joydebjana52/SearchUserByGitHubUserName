331b7b932cd408161771117a05ee1087cf399405
joydebjana52 <joydebjana52@gmail.com>
2023-03-18 22:54:37 +0530

    Simple Javascript project by which you can github user details

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-const fetchData = async () => {
+const submitBtn = document.getElementById("submit-btn");
+const resultDiv = document.getElementById("result");
+
+submitBtn.addEventListener("click", () => {
     const username = document.getElementById("username").value;
+    const apiUrl = `https://api.github.com/users/${username}`;
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            if (data.message) {
+                resultDiv.innerHTML = `<p id="error-message">${data.message}</p>`;
+            } else {
+                const reposUrl = data.repos_url;
+                const imageSrc = data.avatar_url;
+                const repoList = document.createElement("ul");
+                
+                fetch(reposUrl)
+                    .then(response => response.json())
+                    .then(reposData => {
+                        for (const repo of reposData) {
+                            const repoItem = document.createElement("li");
+                            const repoLink = document.createElement("a");
+                            repoLink.href = repo.html_url;
+                            repoLink.textContent = repo.name;
+                            repoItem.appendChild(repoLink);
+                            repoList.appendChild(repoItem);
+                        }
+                        resultDiv.innerHTML = `<img src="${imageSrc}" alt="User Image"><h2>Repositories:</h2>`;
+                        resultDiv.appendChild(repoList);
+                    });
+            }
+        })
+        .catch(error => {
+            resultDiv.innerHTML = `<p id="error-message">${error.message}</p>`;
+        });
+});
 
-    // fetch user data
-    const userDataResponse = await fetch(`https://api.github.com/users/${username}`);
-    const userData = await userDataResponse.json();
 
-    // fetch user repos
-    const userReposResponse = await fetch(`https://api.github.com/users/${username}/repos`);
-    const userRepos = await userReposResponse.json();
 
-    // display user image
-    const userImage = document.getElementById("user-image");
-    userImage.innerHTML = `<img src="${userData.avatar_url}"
